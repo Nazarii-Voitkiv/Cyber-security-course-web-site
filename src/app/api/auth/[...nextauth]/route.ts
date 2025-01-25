d632b5f11a305d2ca2eef10bbe71e4405c6140d7
Nazarii <114947217+Nazarii-Voitkiv@users.noreply.github.com>
2025-01-25 15:27:54 +0100

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
-import type { DefaultSession, NextAuthOptions } from 'next-auth';
+import type { NextAuthOptions } from 'next-auth';
 
 const authOptions: NextAuthOptions = {
   providers: [
