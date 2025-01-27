77fa1ea2b52f320ef3d6769f6392d7d58cb1e40d
Nazarii <114947217+Nazarii-Voitkiv@users.noreply.github.com>
2025-01-28 00:30:03 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,20 +18,38 @@ export default function RootLayout({
 }) {
   return (
     <html lang="uk">
-      <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link
-          rel="apple-touch-icon"
-          href="/apple-touch-icon.png"
-          type="image/png"
-          sizes="180x180"
+    <head>
+        <script
+            dangerouslySetInnerHTML={{
+                __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];
+            t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1790911841722624');
+            fbq('track', 'PageView');
+            `,
+            }}
         />
-      </head>
-      <body className={inter.className}>
-        <FacebookPixel />
-        <Navbar />
-        {children}
-      </body>
+
+        <link rel="icon" href="/favicon.ico" sizes="any"/>
+        <link
+            rel="apple-touch-icon"
+            href="/apple-touch-icon.png"
+            type="image/png"
+            sizes="180x180"
+        />
+    </head>
+    <body className={inter.className}>
+    {/*<FacebookPixel/>*/}
+    <Navbar/>
+    {children}
+    </body>
     </html>
   );
 }
