2db0f6b42d793f8a9745306b0b544332eeadfe5e
Nazarii <114947217+Nazarii-Voitkiv@users.noreply.github.com>
2025-01-28 01:15:29 +0100

    Update FacebookPixel.tsx

--- src/components/FacebookPixel.tsx
+++ src/components/FacebookPixel.tsx
@@ -60,11 +60,12 @@ export default function FacebookPixel() {
                 <img
                     height={1}
                     width={1}
-                    style={{ display: 'none' }}
-                    src="https://www.facebook.com/tr?id=YOUR_PIXEL_ID&ev=PageView&noscript=1"
+                    style={{display: 'none'}}
+                    src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}&ev=PageView&noscript=1`}
                     alt="fbpx"
                 />
             </noscript>
+
         </>
     );
 }
