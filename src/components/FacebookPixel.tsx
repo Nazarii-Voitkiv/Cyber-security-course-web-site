5830cacea3370e8737d02a5c400b03f57cd21ccd
Nazarii <114947217+Nazarii-Voitkiv@users.noreply.github.com>
2025-01-28 01:17:54 +0100

    Update FacebookPixel.tsx

--- src/components/FacebookPixel.tsx
+++ src/components/FacebookPixel.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import Script from 'next/script';
 import { usePathname, useSearchParams } from 'next/navigation';
 import { useEffect, Suspense } from 'react';
+import Script from 'next/script';
 
 declare global {
     interface Window {
@@ -16,46 +16,39 @@ function PageViewTracker() {
     const searchParams = useSearchParams();
 
     useEffect(() => {
-        console.log("Trying to track page view!", pathname, searchParams.toString());
-        if (window.fbq) {
+        if (typeof window !== 'undefined' && window.fbq) {
             window.fbq('track', 'PageView');
-            console.log("PageView event fired to Facebook Pixel!");
-        } else {
-            console.log("fbq is not defined?");
+            console.log('Tracked PageView after route change', pathname);
         }
     }, [pathname, searchParams]);
 
     return null;
 }
 
-
 export default function FacebookPixel() {
     return (
         <>
-            {/* Базовий код пікселя. Виконується після того, як сторінка завантажена (afterInteractive). */}
-            <Script
-                id="facebook-pixel"
-                strategy="afterInteractive"
-                src="https://connect.facebook.net/en_US/fbevents.js"
-                nonce="facebook-pixel-nonce"
-            />
-            <Script
-                id="facebook-pixel-init"
-                strategy="afterInteractive"
-                nonce="facebook-pixel-nonce"
-            >
+            {/* Увесь код – в одному Script */}
+            <Script id="facebook-pixel" strategy="afterInteractive">
                 {`
-                    fbq('init', '${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}');
-                    fbq('track', 'PageView');
-                `}
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];
+          t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window,document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}');
+          fbq('track', 'PageView');
+        `}
             </Script>
 
-            {/* Трекер, який щоразу викликає PageView при зміні маршруту. */}
             <Suspense fallback={null}>
                 <PageViewTracker />
             </Suspense>
 
-            {/* NoScript для тих, хто вимкнув JS (важливо, щоб і ці перегляди відслідковувались). */}
             <noscript>
                 <img
                     height={1}
