4b209dfa0c35168d95e735310a57486a01c629cd
SkillForge177 <114947217+Nazarii-Voitkiv@users.noreply.github.com>
2025-02-18 22:36:22 +0100

    Update route.ts

--- src/app/api/auth/verify/route.ts
+++ src/app/api/auth/verify/route.ts
@@ -1,28 +1,18 @@
-import { NextResponse } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
 import { validateToken } from '@/lib/auth';
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
     try {
-        const cookieHeader = request.headers.get('cookie');
-        if (!cookieHeader) {
-            console.error("No cookie header found.");
-            return NextResponse.json({ error: 'Недійсний токен' }, { status: 401 });
-        }
-        const token = cookieHeader
-            .split('; ')
-            .find(row => row.startsWith('token='))
-            ?.split('=')[1]?.trim() || null;
+        // Використовуємо next API для роботи з кукі:
+        const token = request.cookies.get('token')?.value;
         if (!token) {
-            console.error("Token not set in cookie header.");
+            console.error("Token відсутній у кукі.");
             return NextResponse.json({ error: 'Недійсний токен' }, { status: 401 });
         }
-
         const isValid = await validateToken(token);
-
         if (!isValid) {
             return NextResponse.json({ error: 'Недійсний токен' }, { status: 401 });
         }
-
         return NextResponse.json({ valid: true }, { status: 200 });
     } catch (error) {
         console.error('Помилка перевірки токена:', error);
